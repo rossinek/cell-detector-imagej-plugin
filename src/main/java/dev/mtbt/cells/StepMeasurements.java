7ccde01128a47df006d06001078d6fbc6aecdb58
Artur Rosa <artur.rosa@monterail.com>
2020-07-16 21:50:42 +0200

    new raw data format and length calibration

--- src/main/java/dev/mtbt/cells/StepMeasurements.java
+++ src/main/java/dev/mtbt/cells/StepMeasurements.java
@@ -2,7 +2,6 @@ package dev.mtbt.cells;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.IntStream;
 import javax.swing.Box;
@@ -22,7 +21,6 @@ import ij.ImagePlus;
 import ij.WindowManager;
 import ij.gui.Plot;
 import ij.gui.ProfilePlot;
-import ij.io.SaveDialog;
 import ij.measure.ResultsTable;
 import ij.plugin.filter.GaussianBlur;
 
@@ -33,7 +31,6 @@ public class StepMeasurements implements ICellsPluginStep {
   private JComboBox<String> measurementsCellNameSelect;
 
   private RunnableSpinner fluorescentChannelSpinner;
-  private RunnableSpinner profileDataLengthSpinner;
 
   public StepMeasurements() {
   }
@@ -79,11 +76,6 @@ public class StepMeasurements implements ICellsPluginStep {
     dialogContent.add(new JSeparator());
     dialogContent.add(Box.createVerticalStrut(15));
 
-    this.profileDataLengthSpinner = new RunnableSpinner(200, 10, 1000, () -> {
-    });
-    this.addCenteredComponent(dialogContent, new JLabel("Profile length (#samples)"));
-    this.addCenteredComponent(dialogContent, this.profileDataLengthSpinner);
-
     RunnableButton exportCellDataButton =
         new RunnableButton("Export cell data", this::exportCellData);
     this.addCenteredComponent(dialogContent, exportCellDataButton);
@@ -115,9 +107,10 @@ public class StepMeasurements implements ICellsPluginStep {
 
   protected ResultsTable measureLengths(Cell cell) {
     ResultsTable table = new ResultsTable();
+    double pxSize = this.imp.getCalibration().pixelWidth;
     for (CellFrame frame : cell.getFrames()) {
       table.incrementCounter();
-      table.addValue("Length", frame.getLength());
+      table.addValue("Length", frame.getLength() * pxSize);
     }
     return table;
   }
@@ -141,6 +134,7 @@ public class StepMeasurements implements ICellsPluginStep {
   private ProfilePlot getFrameProfile(Cell cell, int frame, double blur) {
     ImagePlus frameImp = HyperstackHelper.extractFrame(imp,
         (int) this.fluorescentChannelSpinner.getValue(), imp.getSlice(), frame);
+    frameImp.setCalibration(imp.getCalibration());
     if (blur > 0) {
       new GaussianBlur().blurGaussian(frameImp.getProcessor(), blur);
     }
@@ -213,7 +207,7 @@ public class StepMeasurements implements ICellsPluginStep {
     float maxValue = Float.NEGATIVE_INFINITY;
     float minValue = Float.POSITIVE_INFINITY;
     for (int frame = f0; frame < fN; frame++) {
-      ProfilePlot profile = getFrameProfile(cell, frame, 2.0);
+      ProfilePlot profile = this.getFrameProfile(cell, frame, 2.0);
       Plot profilePlot = new Plot("", "", "");
       double[] yValues = profile.getProfile();
       int n = yValues.length;
@@ -256,33 +250,25 @@ public class StepMeasurements implements ICellsPluginStep {
 
   protected void exportCellData() {
     Cell cell = this.getCurrentCell();
-    SaveDialog sd = new SaveDialog("Save Cell Data", "Cell " + cell.getName(), ".csv");
-    String file = sd.getFileName();
-    if (file != null) {
-      try {
-        this.getCellData(cell, (int) this.profileDataLengthSpinner.getValue(), false)
-            .saveAs(sd.getDirectory() + file);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
+    ResultsTable table = this.getCellData(cell, false);
+    table.show("Cell " + cell.getName());
   }
 
-  protected ResultsTable getCellData(Cell cell, int profileLength, boolean addName) {
+  protected ResultsTable getCellData(Cell cell, boolean addName) {
     ResultsTable table = new ResultsTable();
     int f0 = cell.getF0();
     int fN = cell.getFN();
     for (int frame = f0; frame < fN; frame++) {
-      table.incrementCounter();
-      if (addName) {
-        table.addValue("name", cell.getName());
-      }
-      table.addValue("frame", frame);
-      table.addValue("length", cell.getFrame(frame).getLength());
-      double[] profile = getFrameProfile(cell, frame).getProfile();
-      for (int i = 0; i < profileLength; i++) {
-        double profileIndex = ((double) i / profileLength) * profile.length;
-        table.addValue("p" + i, this.sampleData(profile, profileIndex));
+      ProfilePlot plot = this.getFrameProfile(cell, frame);
+      double[] profile = plot.getProfile();
+      float[] positions = plot.getPlot().getXValues();
+      for (int i = 0; i < profile.length; i++) {
+        table.incrementCounter();
+        if (addName) {
+          table.addValue("name", cell.getName());
+        }
+        table.addValue("position", positions[i]);
+        table.addValue("intensity", profile[i]);
       }
     }
     return table;
@@ -298,23 +284,13 @@ public class StepMeasurements implements ICellsPluginStep {
   }
 
   protected void exportAllCellsData() {
-    SaveDialog sd = new SaveDialog("Save Cells Data", "Cells Data", ".csv");
-    String file = sd.getFileName();
     ResultsTable table = new ResultsTable();
-    if (file != null) {
-      try {
-        List<Cell> cells = CellAnalyzer.getAllGenerations(this.cellCollection);
-        for (Cell cell : cells) {
-          ResultsTable cellTable =
-              this.getCellData(cell, (int) this.profileDataLengthSpinner.getValue(), true);
-          ImageJUtils.appendResultsTable(table, cellTable);
-        }
-        table.show("Data table");
-        table.saveAs(sd.getDirectory() + file);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+    List<Cell> cells = CellAnalyzer.getAllGenerations(this.cellCollection);
+    for (Cell cell : cells) {
+      ResultsTable cellTable = this.getCellData(cell, true);
+      ImageJUtils.appendResultsTable(table, cellTable);
     }
+    table.show("Cells Data");
   }
 
   @Override
